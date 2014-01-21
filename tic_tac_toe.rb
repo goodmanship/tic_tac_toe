ec2be6cab53e5c2f143ad4532a55bcec6210ab5e
Rio <rio@lightning-bolt.com>
2014-01-21 10:26:54 -0800

    clean up, add board class, change instance variables to constants

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,36 +1,90 @@
+require 'set'
+WINNERS = [Set[1,2,3],Set[1,4,7],Set[1,5,9],Set[2,5,8],Set[3,5,7],Set[3,6,9],Set[4,5,6],Set[7,8,9]]
+CORNER_CELLS = ['1','3','9','7'] # the order here is important for opposite_corner
+EDGE_CELLS = ['2','4','8','6']
+
+class Board < Array
+  def initialize()
+    self.replace ['1','2','3','4','5','6','7','8','9']
+  end
+
+  def print_board
+    print " #{[self[0..2].join(" | "),self[3..5].join(" | "),self[6..8].join(" | ")].join(" \n---+---+---\n ")} \n"
+  end
+
+  def any_winners?( team )
+    WINNERS.map do |w|
+      if w.subset? team
+        self.print_board
+        p "Winner!"
+        return true
+      end
+    end
+    # if the board's full, tie
+    if self.count('O') + self.count('X') >= 9
+      self.print_board
+      p "Tie!"
+      return true
+    end
+    return false # no winners and no tie
+  end
+
+  def opposite_corner( c )
+    i = CORNER_CELLS.index(c)
+    opp_index = i + 2 > 3 ? i - 2 : i + 2
+    CORNER_CELLS[opp_index]
+  end
+end
+
 class TicTacToe
-  require 'set'
   def initialize(computer_goes_first=false)
-    @winners = [Set[1,2,3],Set[1,4,7],Set[1,5,9],Set[2,5,8],Set[3,5,7],Set[3,6,9],Set[4,5,6],Set[7,8,9]]
-    @board = ['1','2','3','4','5','6','7','8','9']
-    @corner_cells = ['1','3','9','7'] # the order here is important for opposite_corner
-    @edge_cells = ['2','4','8','6']
+    @board = Board.new
     @o = Set.new
     @x = Set.new
 
     computer_move if computer_goes_first
-    print_board
+    @board.print_board
     print "Your turn, enter a cell number:\n"
     move(gets.chomp)
   end
 
+  private
+
+  # game mechanics
+
   def move( cell )
     # check if this is valid input and this cell is available
     if @board.include? cell
-      pencil_in_x(cell.to_i) # also prints an announcement
-      return if any_winners?( @x )
+      pencil_in_x(cell.to_i)
+      return if @board.any_winners?( @x )
 
       p "O moves to #{computer_move}"
-      return if any_winners?( @o )
+      return if @board.any_winners?( @o )
     else
       p "invalid move, try again"
     end
 
-    print_board
+    @board.print_board
     move(gets.chomp)
   end
 
+  def pencil_in_o( cell )
+    @board[cell-1] = "O"
+    @o << cell
+    cell
+  end
+
+  def pencil_in_x( cell )
+    @board[cell-1] = "X"
+    @x << cell
+    p "X moves to #{cell}"
+  end
+
+  # AI methods
+
   def computer_move
+    # Main AI algorithm method
+
     # Wikipedia:
     # Win: If the player has two in a row, they can place a third to get three in a row.
     # Block: If the opponent has two in a row, the player must play the third themself to block the opponent.
@@ -43,40 +97,25 @@ class TicTacToe
     # Empty corner: The player plays in a corner square.
     # Empty side: The player plays in a middle square on any of the 4 sides.
 
-    # AI Algorithm
-
-    # try to win
-    cell = two_in_a_row(@o)
-    # block winning move
-    cell ||= two_in_a_row(@x)
-    # create a fork
-    cell ||= take_fork(@o)
-    # block a fork
-    cell ||= block_fork
-    # take the center
-    cell ||= pencil_in_o(5) if @board.include? '5'
-    # take opposite corner
-    cell ||= take_opposite_corner
-    # take any corner
-    cell ||= take_free_corner
-    # take an edge
-    cell ||= take_free_edge
-
-    # Choose randomly:
-    # a = @board.select{ |t| t.to_i > 0 && t.to_i < 10 }
-    # m = rand(a.length-1)
-    # pencil_in_o( a[m].to_i )
+    cell = two_in_a_row(@o) # try to win
+    cell ||= two_in_a_row(@x) # block winning move
+    cell ||= take_fork(@o) # create a fork
+    cell ||= block_fork # block a fork
+    cell ||= pencil_in_o(5) if @board.include? '5' # take the center
+    cell ||= take_opposite_corner # take opposite corner
+    cell ||= check_cells(CORNER_CELLS) # take any corner
+    cell ||= check_cells(EDGE_CELLS) # take an edge
   end
 
   def block_fork
-    # this method will return the cell required to create a fork for @o or nil
+    # this method will return the cell required to block opponent's fork or nil
     fork_cells = Set.new
     @board.map do |c|
       if !(c=="O" || c=="X")
         test_set = @x.|(Set[c.to_i])
         # check for opportunities (two in a row)
         opportunities = 0
-        @winners.map do |w|
+        WINNERS.map do |w|
           if test_set.&(w).size > 1 # two in a row
             third = w.-(test_set).first # the third
             if @board.include? third.to_s # check if the third is free
@@ -89,11 +128,8 @@ class TicTacToe
         end
       end
     end
-
     return nil if fork_cells.empty? # if there are no forks to block, move on
-
     candidate = force_away_from fork_cells
-
     if candidate
       return candidate
     else
@@ -106,11 +142,10 @@ class TicTacToe
     @board.map do |c|
       if !(c=="O" || c=="X") && !cells.include?(c.to_i) # don't block the fork directly (yet)
         test_set = @o.|(Set[c.to_i])
-        @winners.map do |w|
+        WINNERS.map do |w|
           if test_set.&(w).size > 1 # two in a row
             third = w.-(test_set).first # the third
             if @board.include?(third.to_s) && !cells.include?(third) # check if the third is on the board AND not one of the bad cells
-              p "FORK AVOIDED"
               pencil_in_o(third)
               return third
             end
@@ -128,7 +163,7 @@ class TicTacToe
         test_set = team.|(Set[c.to_i])
         # check for opportunities (two in a row)
         opportunities = 0
-        @winners.map do |w|
+        WINNERS.map do |w|
           if test_set.&(w).size > 1 # two in a row
             third = w.-(test_set).first # the third
             if @board.include? third.to_s # check if the third is free
@@ -137,7 +172,6 @@ class TicTacToe
           end
         end
         if opportunities > 1
-          p "FORK"
           pencil_in_o(c.to_i)
           return c.to_i
         end
@@ -145,13 +179,9 @@ class TicTacToe
     end
     nil
   end
-  
-  def has_fork( test_set )
-    
-  end
 
   def two_in_a_row( team )
-    @winners.map do |w|
+    WINNERS.map do |w|
       if team.&(w).size > 1 # two in a row
         cell = w.-(team).first # the third
         if @board.include? cell.to_s # check if the third is free
@@ -164,9 +194,9 @@ class TicTacToe
   end
 
   def take_opposite_corner
-    @corner_cells.map do |c|
+    CORNER_CELLS.map do |c|
       if @x.include? c.to_i
-        cell = opposite_corner(c)
+        cell = @board.opposite_corner(c)
         if @board.include? cell
           pencil_in_o(cell.to_i)
           return cell.to_i
@@ -174,20 +204,6 @@ class TicTacToe
       end
     end
     return nil
-  end
-
-  def opposite_corner( c )
-    i = @corner_cells.index(c)
-    opp_index = i + 2 > 3 ? i - 2 : i + 2
-    @corner_cells[opp_index]
-  end
-
-  def take_free_corner
-    check_cells(@corner_cells)
-  end
-  
-  def take_free_edge
-    check_cells(@edge_cells)
   end
   
   def check_cells( a )
@@ -198,39 +214,6 @@ class TicTacToe
       end
     end
     return nil
-  end
-
-  def any_winners?( team )
-    @winners.map do |w|
-      if w.subset? team
-        print_board
-        p "Winner!"
-        return true
-      end
-    end
-    # if the board's full, tie
-    if @x.length + @o.length >= 9
-      print_board
-      p "Tie!"
-      return true
-    end
-    return false # no winners and no tie
-  end
-
-  def pencil_in_o( cell )
-    @board[cell-1] = "O"
-    @o << cell
-    cell
-  end
-
-  def pencil_in_x( cell )
-    @board[cell-1] = "X"
-    @x << cell
-    p "X moves to #{cell}"
-  end
-
-  def print_board
-    print " #{[@board[0..2].join(" | "),@board[3..5].join(" | "),@board[6..8].join(" | ")].join(" \n---+---+---\n ")} \n"
   end
 end
 
